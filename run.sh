738dc40fa7681a45779522b6ec1e72c57e280257
Omar Nova <62707079+OmarNova@users.noreply.github.com>
2023-10-10 18:27:44 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #docker run --name backpy -d -p 80:8050 backpy
-cp /home/distribuidos/.env /home/distribuidos4/actions-runner/_work/Auth-Server/Auth-Server/VF-Server
+cp /home/distribuidos4/.env /home/distribuidos4/actions-runner/_work/Auth-Server/Auth-Server/VF-Server
 docker compose up -d --build
